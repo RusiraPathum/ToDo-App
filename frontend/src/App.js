895798bde55046683def61b9ec3a21663f1eb372
it18218640 <it18218640@my.sliit.lk>
2022-03-02 16:09:44 +0530

    add todo table list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ToDoTable from './component/ToDoTable';
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-     
+      <ToDoTable/>
     </div>
   );
 }
